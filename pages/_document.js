2c3d2728a49c65dcf75c6b0d036403915b73b728
RavelloH <ravellohh@gmail.com>
2025-03-23 21:38:23 +0800

    Opti. 兼容性改进

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
-          {/* Umami 统计脚本 - 请替换为您的Umami统计代码 */}
-          <script async defer data-website-id="your-website-id" src="https://your-umami-url/umami.js"></script>
+          <script defer src="https://analytics.ravelloh.top/script.js" data-website-id="8ef18ba2-4313-457b-b6f1-a36ef1e1a27c"></script>
         </Head>
         <body>
           <Main />
